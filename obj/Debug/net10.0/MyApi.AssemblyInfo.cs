239f8b4d2f19e0073bc57b9531cbc69f335bd3e2
Timileyin <akinnolatimileyin@gmail.com>
2026-06-10 03:54:57 -0700

    remove artifacts

--- obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c9310592c8a331de2575312cfba64c5560c824")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ee100d2122482579046d74aa85f6ac0156007a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
